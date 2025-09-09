58e205dbef93aa9f71116b9ca7d0b4f67acd955f
Abuzar <hamzamushtaq362@gmail.com>
2025-09-09 12:00:38 +0500

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const axiosi = axios.create({
   withCredentials: true,
   // baseURL: "http://localhost:8000",
-  baseURL: "https://ms-traders-b.vercel.app/",
+  baseURL: "https://ms-traders-b.vercel.app",
 });
